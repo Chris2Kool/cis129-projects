d859261f7bc061bbeca154a37c53fd82380828e3
Chris2Kool <cvalle3@mail.pima.edu>
2024-09-19 18:10:17 -0700

    started my first lab with writing a small story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Christian Valle 
+# My first Python lab in my CIS129 course
+# Prints a simple short stroy using print()
+
+print("I am a young boy that has a super power)
+print("I have the ability to fly")
+print("Everyday I get to go around the world")
+print("I pick and spot and go")
+print("It is the most fun I have had in ages")
