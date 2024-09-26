84807e02c2577978a7da2f359d90da858aad93e5
Chris2Kool <cvalle3@mail.pima.edu>
2024-09-26 12:48:58 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -2,8 +2,8 @@
 # My first Python lab in my CIS129 course
 # Prints a simple short stroy using print()
 
-print("I am a young boy that has a super power")
-print("I have the ability to fly")
-print("Everyday I get to go around the world")
-print("I pick and spot and go")
-print("It is the most fun I have had in ages")
+print('I am a young boy that just got a super power')
+print('I have the ability to fly')
+print('Everyday I get to go around the world')
+print('I pick and spot and go')
+print('It is the most fun I have had in ages')
